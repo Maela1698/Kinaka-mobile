575c69cfa9e670a16d610241d087ef1e4e41ef2b
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2024-01-12 14:37:53 +0300

    yess

--- src/pages/Inscription.tsx
+++ src/pages/Inscription.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { IonButton, IonInput, IonItem, IonList, IonText } from '@ionic/react';
+import { IonButton, IonContent, IonInput, IonItem, IonList, IonText } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
+import './style/main.css';
+
 
 
 const Inscription: React.FC = () => {
@@ -12,15 +14,17 @@ const Inscription: React.FC = () => {
     };
     return(
         <>
-            <IonInput label="Nom Utilisatieur" labelPlacement="floating" fill="outline" required/>
-            <br/>
-            <IonInput label="Mail" labelPlacement="floating" fill="outline" type="email" required/>
-            <br/>
-            <IonInput label="Mot de passe" labelPlacement="floating" fill="outline" type="password" required/>
-            <br/>
-            <IonInput label="Date de naissance" labelPlacement="floating" fill="outline" type="date" required/>
-            <br/>
-            <IonButton onClick={handleInscriptionClick}>S'INSCRIRE</IonButton>
+            <IonContent className="ion-padding">
+                <IonInput label="Nom Utilisatieur" labelPlacement="floating" fill="outline" required/>
+                <br/>
+                <IonInput label="Mail" labelPlacement="floating" fill="outline" type="email" required/>
+                <br/>
+                <IonInput label="Mot de passe" labelPlacement="floating" fill="outline" type="password" required/>
+                <br/>
+                <IonInput label="Date de naissance" labelPlacement="floating" fill="outline" type="date" required/>
+                <br/>
+                <IonButton onClick={handleInscriptionClick}>S'INSCRIRE</IonButton>
+            </IonContent>
         </>
     );
 };
