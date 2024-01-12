8acbfc1c2d041317f19eeabb4bf9833620b4904f
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2024-01-12 14:48:35 +0300

    commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,67 +1,45 @@
-import { IonButton, IonContent, IonGrid, IonInput, IonRow, IonCol, IonTitle } from '@ionic/react';
-import React, { useState } from 'react';
-import { useHistory } from 'react-router';
-
+import React from 'react';
+import { IonButton, IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonInput, IonItem, IonList, IonRow, IonText, IonTitle } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+import './style/main.css';
 import './style/login.css';
 
-const Login = () => {
+
+
+
+const Login: React.FC = () => {
     const history = useHistory();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const isButtonDisabled = email.trim() === '' || password.trim() === '';
+    const handleInscriptionClick = () => {
+        // Redirige vers la page d'inscription
+        history.push('/inscription');
+    };
 
-  const handleLoginClick = () => {
-    if (!isButtonDisabled) {
-      // Effectuez l'action de connexion ici
-      console.log('Se connecter avec:', email, password);
-    }
-  };
+    const goAcceuil = () => {
+        // Redirige vers la page d'inscription
+        history.push('/acceuil');
+    };
 
-const handleInscriptionClick = () => {
-    // Redirige vers la page d'inscription
-    history.push('/login');
+    return(
+        <>  
+            <IonContent className="ion-padding">
+                <IonGrid>
+                    <IonRow>
+                        <IonCol size="12" size-md="6" offset-md="3">
+                            <IonTitle className="ion-text-center ion-margin-top kinaka-title">KINAKA</IonTitle> 
+                            <div className="ion-margin ion-margin-lg">
+                            <IonInput label="Mail" labelPlacement="floating" fill="outline" type="email" required/>
+                            <br/>
+                            <IonInput label="Mot de Passe" labelPlacement="floating" fill="outline" type="password" required/>
+                            <br/>
+                            <IonButton expand="full" onClick={goAcceuil} className="ion-margin-top">SE CONNECTER</IonButton>
+                            <IonButton expand="full" className="ion-margin-top secondary" onClick={handleInscriptionClick}>S'INSCRIRE</IonButton>
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
+        </>
+    );
 };
-
-  return (
-    <IonContent className="ion-padding">
-      <IonGrid className="">
-        <IonRow>
-          <IonCol size="12" size-md="6" offset-md="3">
-            <IonTitle className="ion-text-center ion-margin-top kinaka-title">KINAKA</IonTitle>
-            <div className="ion-margin ion-margin-lg">
-              <IonInput
-                label="Mail"
-                labelPlacement="floating"
-                fill="outline"
-                type="email"
-                required
-                value={email}
-                onIonChange={(e) => setEmail(e.detail.value!)}
-              />
-              <br />
-              <IonInput
-                label="Mot de Passe"
-                labelPlacement="floating"
-                fill="outline"
-                type="password"
-                required
-                value={password}
-                onIonChange={(e) => setPassword(e.detail.value!)}
-              />
-              <br />
-              <IonButton expand="full" onClick={handleLoginClick} disabled={isButtonDisabled} className="ion-margin-top">
-                SE CONNECTER
-              </IonButton>
-              <IonButton expand="full" className="ion-margin-top secondary" onClick={handleInscriptionClick}>
-                S'INSCRIRE
-              </IonButton>
-            </div>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </IonContent>
-  );
-};
-
 export default Login;
