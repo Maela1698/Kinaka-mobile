7042215a6ad45f69ca970376b9b6229db72b5825
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2024-01-12 17:46:57 +0300

    commit first

--- src/pages/Inscription.tsx
+++ src/pages/Inscription.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { IonButton, IonContent, IonInput, IonItem, IonList, IonText } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonInput, IonItem, IonList, IonRow, IonText, IonTitle } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
-import './style/main.css';
 
 
 
@@ -15,15 +14,25 @@ const Inscription: React.FC = () => {
     return(
         <>
             <IonContent className="ion-padding">
-                <IonInput label="Nom Utilisatieur" labelPlacement="floating" fill="outline" required/>
-                <br/>
-                <IonInput label="Mail" labelPlacement="floating" fill="outline" type="email" required/>
-                <br/>
-                <IonInput label="Mot de passe" labelPlacement="floating" fill="outline" type="password" required/>
-                <br/>
-                <IonInput label="Date de naissance" labelPlacement="floating" fill="outline" type="date" required/>
-                <br/>
-                <IonButton onClick={handleInscriptionClick}>S'INSCRIRE</IonButton>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol size="12" size-md="6" offset-md="3">
+                            <IonTitle className="ion-text-center ion-margin-top kinaka-title"><h1>KINAKA</h1></IonTitle> 
+                            <h3 className="ion-text-center">Inscription</h3>
+                            <div className="ion-margin ion-margin-lg">
+                                <IonInput label="Nom Utilisateur" labelPlacement="floating" fill="outline"   />
+                                <br/>
+                                <IonInput label="Mail" labelPlacement="floating" fill="outline" type="email" placeholder="jhondoe@gmail.com" />
+                                <br/>
+                                <IonInput label="Mot de Passe" labelPlacement="floating" fill="outline" type="password" placeholder="mot de passe"/>
+                                <br/>
+                                <IonInput label="Date de Naissance" labelPlacement="floating" fill="outline" type="date"/>
+                                <br/>
+                                <IonButton expand="full" className="ion-margin-top secondary" onClick={handleInscriptionClick}>S'INSCRIRE</IonButton>
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
             </IonContent>
         </>
     );
