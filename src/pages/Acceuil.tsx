c0a5779f26132802af752c37c622227051037bd9
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2024-01-12 13:27:10 +0300

    commit

--- src/pages/Acceuil.tsx
+++ src/pages/Acceuil.tsx
@@ -20,7 +20,7 @@ function Acceuil() {
             <IonTitle>Kinaka</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent className="ion-padding">Bonjour Mr Ano tay pory fa mety</IonContent>
+        <IonContent className="ion-padding">Bonjour Mr Ano tay pory fa mety huhuhu</IonContent>
       </IonPage>
     </>
   );
