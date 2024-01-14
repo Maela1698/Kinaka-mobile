1c53f1a5660dab3855e51c02d5796443e0531737
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2024-01-14 18:12:50 +0300

    acceuil

--- src/pages/Acceuil.tsx
+++ src/pages/Acceuil.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonMenu, IonMenuButton, IonPage, IonRadio, IonRadioGroup, IonRow, IonSelect, IonSelectOption, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import './style/acceuil.css'
 
 function Acceuil() {
+ 
+  const [vitesse, setVitesse] = useState<string>('');
+  const [moteur, setMoteur] = useState<string>('');
+
   return (
     <>
       <IonMenu contentId="main-content">
@@ -28,7 +33,102 @@ function Acceuil() {
               <IonCardSubtitle>Bienvenue sur KINAKA</IonCardSubtitle>
             </IonCardHeader>
 
-            <IonCardContent>Tirez parti de fonctionnalités intuitives pour télécharger des photos attrayantes de votre voiture, attirant ainsi l'attention des acheteurs. Vous avez également la possibilité de définir un prix compétitif et d'ajouter des informations supplémentaires pour mettre en valeur les caractéristiques uniques de votre véhicule.</IonCardContent>
+            <IonCardContent>Tirez parti de fonctionnalités intuitives pour télécharger des photos attrayantes de votre voiture, attirant ainsi l'attention des acheteurs. </IonCardContent>
+          </IonCard>
+          <br/>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>Ajout Annonce</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonList>
+                <IonItem>
+                  <IonSelect label="Categorie" placeholder="Voiture de luxe">
+                    <IonSelectOption value="Voiture de luxe">Voiture de luxe</IonSelectOption>
+                    <IonSelectOption value="Voiture vintage">Voiture vintage</IonSelectOption>
+                    <IonSelectOption value="Camping car">Camping car</IonSelectOption>
+                    <IonSelectOption value="Coupe">Coupe</IonSelectOption>
+                  </IonSelect>
+                </IonItem>
+                <IonItem>
+                  <IonSelect label="Marque" placeholder="Mercedes">
+                    <IonSelectOption value="Mercedes">Mercedes</IonSelectOption>
+                    <IonSelectOption value="Peugeot">Peugeot</IonSelectOption>
+                    <IonSelectOption value="Toyota">Toyota</IonSelectOption>
+                  </IonSelect>
+                </IonItem>
+                <IonItem>
+                  <IonSelect label="Etat" placeholder="Bonne occasion">
+                    <IonSelectOption value="Bonne occasion">Bonne occasion</IonSelectOption>
+                    <IonSelectOption value="Comme neuve">Comme neuve</IonSelectOption>
+                    <IonSelectOption value="Neuve">Neuve</IonSelectOption>
+                  </IonSelect>
+                </IonItem>
+              </IonList>
+              <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <div className="">
+                            <IonInput label="Kilometrage" labelPlacement="floating" fill="outline" type="number" className="input-color-black"/>
+                            
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                <IonList>
+                  <IonItemDivider>
+                    <IonLabel className="input-color-black">Vitesse</IonLabel>
+                  </IonItemDivider>
+
+                  <IonRadioGroup value={vitesse} onIonChange={e => setVitesse(e.detail.value)}>
+                    <IonItem>
+                      <IonLabel>Manuelle</IonLabel>
+                      <IonRadio slot="start" value="manuelle" />
+                    </IonItem>
+
+                    <IonItem>
+                      <IonLabel>Automatique</IonLabel>
+                      <IonRadio slot="start" value="automatique" />
+                    </IonItem>
+                  </IonRadioGroup>
+                </IonList>
+                <IonList>
+                  <IonItemDivider>
+                    <IonLabel className="input-color-black">Moteur</IonLabel>
+                  </IonItemDivider>
+
+                  <IonRadioGroup value={moteur} onIonChange={e => setMoteur(e.detail.value)}>
+                    <IonItem>
+                      <IonLabel>Diesel</IonLabel>
+                      <IonRadio slot="start" value="diesel" />
+                    </IonItem>
+
+                    <IonItem>
+                      <IonLabel>Essence</IonLabel>
+                      <IonRadio slot="start" value="essence" />
+                    </IonItem>
+                    <IonItem>
+                      <IonLabel>Hybride</IonLabel>
+                      <IonRadio slot="start" value="hybride" />
+                    </IonItem>
+                    <IonItem>
+                      <IonLabel>Electronique</IonLabel>
+                      <IonRadio slot="start" value="Electronique" />
+                    </IonItem>
+                  </IonRadioGroup>
+                </IonList>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <div className="">
+                            <IonInput label="Prix" labelPlacement="floating" fill="outline" type="number" className="input-color-black"/>
+                            
+                            </div>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                <IonButton expand="full" className="ion-margin-top secondary" >AJOUTER ANNONCE</IonButton>
+            </IonCardContent>  
           </IonCard>
         </IonContent>
       </IonPage>
